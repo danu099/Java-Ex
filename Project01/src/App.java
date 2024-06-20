8c4c1bcdcaa8efe2158a63c2c10ff6d9ed5d88c6
danu099 <danushapramode99@gmail.com>
2024-06-20 09:12:09 +0530

    Second Commit

--- Project01/src/App.java
+++ Project01/src/App.java
@@ -1,5 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");     // output
-    }
+    public static void main(String[] args) {
+        car c1 = new car();  //create object
+		
+		c1.color = "Red";    //initialize attributes
+		c1.name = "Toyota";
+		c1.speed = 150;
+		c1.weight = 45.3;
+		c1.no = 123;
+		
+		c1.speed();  //Call methods from car class
+		c1.color();
+		c1.weight();
+		c1.name();
+
+	}
+
 }
--- Project01/src/car.java
+++ Project01/src/car.java
@@ -1,3 +1,27 @@
 public class car {
+	int no;                   //instance variables
+	String name;   
+	String color;
+	double weight;
+	int speed;
+	
+	
+	public void speed() {              //instance methods
+		System.out.println(speed);
+	}
+	
+	public void color() {
+		System.out.println(color);
+	}
 
+	public void weight() {
+		System.out.println(weight);
+		
+		
+	}
+	
+	public void name() {
+		System.out.println(name);
+		
+	}
 }
